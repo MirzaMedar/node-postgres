aaf6ee1f37b0912558c421feac5d4fb53b0f9f20
MirzaMedar <mirza.medar@edu.fit.ba>
2018-12-10 19:51:54 +0100

    Code format

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var studentsRouter = require('./routes/students');
 
 var app = express();
@@ -15,7 +14,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/students', studentsRouter);
 
 module.exports = app;
